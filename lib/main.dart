667fb8000e03e34a62ada3bd025357e0f5f2bb93
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-03-29 02:11:28 +0600

    feat: lock app orientation to portrait
    Set preferred device orientations to only portrait up to ensure consistent layout and prevent unwanted rotations during app usage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+    await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   // Initialize services here
   // await initServices();
   
